2f9acc21990919d16d2a597fc1409d87dd3970ee
Maycol Sanchez Salazar <msanchez@sermepre.com>
2026-08-20 15:26:44 -0500

    fix(vite): fix public assets base configuration for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.GITHUB_PAGES ? '/singular-page/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
